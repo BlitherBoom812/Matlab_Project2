5268b7a19c43e39ef12fbc356e65306d18d6f99d
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2023-09-05 19:08:30 +0800

    4_8

--- hw_4_8.m
+++ hw_4_8.m
@@ -5,10 +5,26 @@ clc;
 
 dir = "./图像处理所需资源/";
 load(strcat(dir, "hall.mat"));
+load(strcat(dir, "JpegCoeff.mat"));
 
 dct2P = double(hall_gray) - 128;
-dct2C = blockproc(dct2P, [8, 8], @dct2);
+dct2C = blockproc(dct2P, [8, 8], @(blk) dct2(blk.data));
+dct2C_tilde = blockproc(dct2C, [8, 8], @(blk) quantify_block(blk.data, QTAB));
+dct2C_tilde = blockproc(dct2C_tilde, [8, 8], @(blk) zigzagScan(blk.data)');
 
-function Q = quantify_block(block)
 
+[rows, cols] = size(dct2C_tilde);
+dct2C_tilde_final = [];
+for i = 1:(rows/64)
+    for j = 1:cols
+        if i == 1 && j == 1
+            dct2C_tilde_final = dct2C_tilde(1:64, 1);
+        else
+            dct2C_tilde_final = [dct2C_tilde_final, dct2C_tilde((i - 1) * 64 + 1:i * 64, j)];
+        end
+    end
+end
+
+function Q = quantify_block(block, quantify)
+    Q = round(block ./ quantify);
 end
