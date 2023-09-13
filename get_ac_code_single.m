2176dc011b4d5d418935b5fd2f580ddfd07ba577
BlitherBoom812 <tsz21@mails.tsinghua.edu.cn>
2023-09-13 12:43:33 +0800

    4_9

---
+++ get_ac_code_single.m
@@ -0,0 +1,33 @@
+
+function ac_code_single = get_ac_code_single(single_block, ACTAB)
+    ZRL = [1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1];
+    EOB = [1, 0, 1, 0];
+    zero_num = 0;
+    zrl_num = 0;
+    ac_code_single = [];
+    for scan_idx = 2:length(single_block)
+        if single_block(scan_idx) == 0
+            if zero_num == 15
+                zrl_num = zrl_num + 1;
+                zero_num = 0;
+            else
+                zero_num = zero_num + 1;
+            end
+        else 
+            if zrl_num > 0
+                ac_code_single = [ac_code_single, repmat(ZRL, 1, zrl_num)];
+                zrl_num = 0;
+            end
+            data = single_block(scan_idx);
+            amp = my_d2b(data);
+            size = length(amp);
+            run = zero_num;
+            actab_row = ACTAB(run * 10 + size, :);
+            actab_looked_up = actab_row(4:actab_row(3)+3);
+            ac_code_single = [ac_code_single, actab_looked_up, amp];
+            % fprintf("data = %d, amp = %s, size = %s, run = %s, actab_looked = %s\n", data, mat2str(amp), mat2str(size), mat2str(run), mat2str(actab_looked_up));
+            zero_num = 0;
+        end
+    end
+    ac_code_single = [ac_code_single, EOB];
+end
--- hw_4_9.m
+++ hw_4_9.m
@@ -10,3 +10,14 @@ load(strcat(dir, "JpegCoeff.mat"));
 dct2C_tilde = get_dct2C_tilde(hall_gray, QTAB);
 dct2C_tilde_final = get_dct2C_tilde_final(dct2C_tilde);
 dc_code = get_dc_code(dct2C_tilde_final, DCTAB);
+
+ac_code = [];
+for i = 1:length(dct2C_tilde_final)
+    single_block = dct2C_tilde_final(:, i);
+    ac_code_single = get_ac_code_single(single_block, ACTAB);
+    ac_code = [ac_code, ac_code_single];
+end
+
+[row, col] = size(hall_gray);
+
+save('jpegcode.mat', "col", "row", "ac_code", "dc_code")
